bf2e75ad4fe78d03b4672a2e076a7bc123a5db69
Mark Glenn <mglenn@cox.net>
2022-06-18 09:09:13 -0700

    remove build script

--- build_skcc_skimmerEXE.bat
+++
@@ -1,9 +0,0 @@
-::
-:: Requires PyInstaller.  Install using command:
-::   pip install PyInstaller
-::
-
-python3 -m PyInstaller --onefile --distpath=. --workpath=tempdir skcc_skimmer.py
-
-del skcc_skimmer.spec
-rmdir /S /Q tempdir
