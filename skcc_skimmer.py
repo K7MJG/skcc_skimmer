56f925b8148f75334e2ca88b6aaa790f07849b53
Mark Glenn <mglenn@cox.net>
2025-03-03 16:05:18 -0700

    integrate cConfig.py with AI rewrite

--- skcc_skimmer.py
+++ skcc_skimmer.py
@@ -77,11 +77,11 @@ from datetime import timedelta
 from datetime import datetime
 
 from types         import FrameType
-from typing        import Any, NoReturn, Literal
+from typing        import Any, NoReturn, Literal, TypedDict, get_args
 
 from math          import radians, sin, cos, atan2, sqrt
 
-from Lib.cConfig       import cConfig
+import argparse
 from collections.abc   import AsyncGenerator
 
 import asyncio
@@ -112,6 +112,456 @@ def Miles2Km(Miles: int) -> int:
 
 def Stripped(text: str) -> str:
     return ''.join([c for c in text if 31 < ord(c) < 127])
+
+#!/usr/bin/python3
+'''
+
+     The MIT License (MIT)
+
+     Copyright (c) 2015-2023 Mark J Glenn
+
+     Permission is hereby granted, free of charge, to any person obtaining a copy
+     of this software and associated documentation files (the "Software"), to deal
+     in the Software without restriction, including without limitation the rights
+     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     copies of the Software, and to permit persons to whom the Software is
+     furnished to do so, subject to the following conditions:
+
+     The above copyright notice and this permission notice shall be included in all
+     copies or substantial portions of the Software.
+
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+     SOFTWARE.
+
+     Mark Glenn
+     mglenn@cox.net
+
+'''
+
+class cConfig:
+    class cProgressDots:
+        ENABLED:         bool
+        DISPLAY_SECONDS: int
+        DOTS_PER_LINE:   int
+    PROGRESS_DOTS = cProgressDots()
+
+    class cLogFile:
+        FILE_NAME:         str
+        ENABLED:           bool
+        LOG_FILE:          str
+        DELETE_ON_STARTUP: bool
+    LOG_FILE = cLogFile()
+
+    class cHighWpm:
+        tAction = Literal['suppress', 'warn', 'always-display']
+        ACTION: tAction
+        THRESHOLD: int
+    HIGH_WPM = cHighWpm
+
+    class cOffFrequency:
+        ACTION:    Literal['suppress', 'warn']
+        TOLERANCE: int
+    OFF_FREQUENCY = cOffFrequency()
+
+    class cSked:
+        ENABLED:       bool
+        CHECK_SECONDS: int
+    SKED = cSked
+
+    class cNotification:
+        ENABLED:                      bool
+        CONDITION:                    list[str]   # list[Literal['goals', 'targets', 'friends']]
+        RENOTIFICATION_DELAY_SECONDS: int
+    NOTIFICATION = cNotification
+
+
+    MY_CALLSIGN:              str
+    ADI_FILE:                 str
+    MY_GRIDSQUARE:            str
+    GOALS:                    list[str]
+    TARGETS:                  list[str]
+    BANDS:                    list[int]
+    FRIENDS:                  list[str]
+    EXCLUSIONS:               list[str]
+    DISTANCE_UNITS:           str
+    SPOT_PERSISTENCE_MINUTES: int
+    VERBOSE:                  bool
+    LOG_BAD_SPOTS:            bool
+    SPOTTER_RADIUS:           int
+    K3Y_YEAR:                 int
+
+    configFile: dict[str, Any]
+
+    class TypedConfig(TypedDict):
+        HIGH_WPM: 'cConfig.cHighWpm'
+
+
+    configFile2: TypedConfig
+
+    def __init__(self, ArgV: list[str]):
+        def ReadSkccSkimmerCfg() -> dict[str, Any]:
+            config_vars: dict[str, Any] = {}
+
+            with open('skcc_skimmer.cfg', 'r', encoding='utf-8') as configFile:
+                ConfigFileString = configFile.read()
+                exec(ConfigFileString, {}, config_vars)
+
+            return config_vars
+
+        self.configFile = ReadSkccSkimmerCfg()
+
+        if 'MY_CALLSIGN' in self.configFile:
+            self.MY_CALLSIGN = self.configFile['MY_CALLSIGN']
+
+        if 'ADI_FILE' in self.configFile:
+            self.ADI_FILE = self.configFile['ADI_FILE']
+
+        if 'MY_GRIDSQUARE' in self.configFile:
+            self.MY_GRIDSQUARE = self.configFile['MY_GRIDSQUARE']
+
+        if 'SPOTTER_RADIUS' in self.configFile:
+            self.SPOTTER_RADIUS = int(self.configFile['SPOTTER_RADIUS'])
+
+        if 'GOALS' in self.configFile:
+            self.GOALS = self.Parse(self.configFile['GOALS'], 'C CXN T TXN S SXN WAS WAS-C WAS-T WAS-S P BRAG K3Y', 'goal')
+
+        if 'TARGETS' in self.configFile:
+            self.TARGETS = self.Parse(self.configFile['TARGETS'], 'C CXN T TXN S SXN', 'target')
+
+        if 'BANDS' in self.configFile:
+            self.BANDS = [int(Band)  for Band in Split(self.configFile['BANDS'])]
+
+        if 'FRIENDS' in self.configFile:
+            self.FRIENDS = [friend  for friend in Split(self.configFile['FRIENDS'])]
+
+        if 'EXCLUSIONS' in self.configFile:
+            self.EXCLUSIONS = [friend  for friend in Split(self.configFile['EXCLUSIONS'])]
+
+        if 'LOG_FILE' in self.configFile:
+            logFile = self.configFile['LOG_FILE']
+
+            if 'ENABLED' in logFile:
+                self.LOG_FILE.ENABLED = bool(logFile['ENABLED'])
+
+            if 'FILE_NAME' in logFile:
+                self.LOG_FILE.FILE_NAME = logFile['FILE_NAME']
+
+            if 'DELETE_ON_STARTUP' in logFile:
+                self.LOG_FILE.DELETE_ON_STARTUP = logFile['DELETE_ON_STARTUP']
+
+
+        if 'PROGRESS_DOTS' in self.configFile:
+            progressDots = self.configFile['PROGRESS_DOTS']
+
+            if 'ENABLED' in progressDots:
+                self.PROGRESS_DOTS.ENABLED = bool(progressDots['ENABLED'])
+
+            if 'DISPLAY_SECONDS' in progressDots:
+                self.PROGRESS_DOTS.DISPLAY_SECONDS = progressDots['DISPLAY_SECONDS']
+
+            if 'DOTS_PER_LINE' in progressDots:
+                self.PROGRESS_DOTS.DOTS_PER_LINE = progressDots['DOTS_PER_LINE']
+
+        if 'SKED' in self.configFile:
+            sked = self.configFile['SKED']
+
+            if 'ENABLED' in sked:
+                self.SKED.ENABLED = bool(sked['ENABLED'])
+
+            if 'CHECK_SECONDS' in sked:
+                self.SKED.CHECK_SECONDS = int(sked['CHECK_SECONDS'])
+
+        if 'OFF_FREQUENCY' in self.configFile:
+            offFrequency = self.configFile['OFF_FREQUENCY']
+
+            if 'ACTION' in offFrequency:
+                self.OFF_FREQUENCY.ACTION = offFrequency['ACTION']
+
+            if 'TOLERANCE' in offFrequency:
+                self.OFF_FREQUENCY.TOLERANCE = int(offFrequency['TOLERANCE'])
+
+        if 'HIGH_WPM' in self.configFile:
+            highWpm = self.configFile['HIGH_WPM']
+
+            if 'ACTION' in highWpm:
+                action: cConfig.cHighWpm.tAction = highWpm['ACTION']
+
+                if action not in get_args(cConfig.cHighWpm.tAction):
+                    print(f"Must be one of {get_args(cConfig.cHighWpm.tAction)}.")
+
+                self.HIGH_WPM.ACTION = action
+
+            if 'THRESHOLD' in highWpm:
+                self.HIGH_WPM.THRESHOLD = int(highWpm['THRESHOLD'])
+
+        if 'NOTIFICATION' in self.configFile:
+            notification = self.configFile['NOTIFICATION']
+
+            if 'ENABLED' in notification:
+                self.NOTIFICATION.ENABLED = bool(notification['ENABLED'])
+
+            if 'CONDITION' in notification:
+                conditions = Split(notification['CONDITION'])
+
+                for condition in conditions:
+                    if condition not in ['goals', 'targets', 'friends']:
+                        print(f"NOTIFICATION CONDITION '{condition}' must be 'goals' and/or 'targets' and/or 'friends'")
+                        sys.exit()
+
+                self.NOTIFICATION.CONDITION = conditions
+
+            if 'THRESHOLD' in notification:
+                self.NOTIFICATION.RENOTIFICATION_DELAY_SECONDS = int(notification['RENOTIFICATION_DELAY_SECONDS'])
+            else:
+                self.NOTIFICATION.RENOTIFICATION_DELAY_SECONDS = 30
+
+        if 'VERBOSE' in self.configFile:
+            self.VERBOSE = bool(self.configFile['VERBOSE'])
+        else:
+            self.VERBOSE = False
+
+        if 'LOG_BAD_SPOTS' in self.configFile:
+            self.LOG_BAD_SPOTS = bool(self.configFile['LOG_BAD_SPOTS'])
+        else:
+            self.LOG_BAD_SPOTS = False
+
+        if 'DISTANCE_UNITS' in self.configFile and self.configFile['DISTANCE_UNITS'] in ('mi', 'km'):
+            self.DISTANCE_UNITS = self.configFile['DISTANCE_UNITS']
+        else:
+            self.DISTANCE_UNITS = 'mi'
+
+        if 'K3Y_YEAR' in self.configFile:
+            self.K3Y_YEAR = self.configFile['K3Y_YEAR']
+        else:
+            self.K3Y_YEAR = datetime.now().year
+
+        self._ParseArgs(ArgV)
+
+        self._ValidateConfig()
+
+    def _ParseArgs(self, ArgV: list[str]) -> None:
+        parser = argparse.ArgumentParser(description="SKCC Skimmer Configuration")
+
+        parser.add_argument("-a", "--adi", type=str, help="ADI file")
+        parser.add_argument("-b", "--bands", type=str, help="Comma-separated bands")
+        parser.add_argument("-B", "--brag-months", type=int, help="Number of months back for bragging")
+        parser.add_argument("-c", "--callsign", type=str, help="Your callsign")
+        parser.add_argument("-d", "--distance-units", type=str, choices=["mi", "km"], help="Distance units (mi/km)")
+        parser.add_argument("-g", "--goals", type=str, help="Goals")
+        parser.add_argument("-i", "--interactive", action="store_true", help="Enable interactive mode")
+        parser.add_argument("-l", "--logfile", type=str, help="Logfile name")
+        parser.add_argument("-m", "--maidenhead", type=str, help="Grid square")
+        parser.add_argument("-n", "--notification", type=str, choices=["on", "off"], help="Enable notifications (on/off)")
+        parser.add_argument("-r", "--radius", type=int, help="Distance radius in miles")
+        parser.add_argument("-s", "--sked", type=str, choices=["on", "off"], help="Enable scheduled mode (on/off)")
+        parser.add_argument("-t", "--targets", type=str, help="Targets")
+        parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
+
+        args = parser.parse_args(ArgV)
+
+        self.INTERACTIVE = args.interactive
+        self.VERBOSE = args.verbose
+
+        if args.adi:
+            self.ADI_FILE = args.adi
+        if args.bands:
+            self.BANDS = [int(band) for band in Split(args.bands)]
+        if args.brag_months:
+            self.BRAG_MONTHS = args.brag_months
+        if args.callsign:
+            self.MY_CALLSIGN = args.callsign.upper()
+        if args.distance_units:
+            self.DISTANCE_UNITS = args.distance_units
+        if args.goals:
+            self.GOALS = self.Parse(args.goals, "C CXN T TXN S SXN WAS WAS-C WAS-T WAS-S P BRAG K3Y", "goal")
+        if args.logfile:
+            self.LOG_FILE.ENABLED = True
+            self.LOG_FILE.DELETE_ON_STARTUP = True
+            self.LOG_FILE.FILE_NAME = args.logfile
+        if args.maidenhead:
+            self.MY_GRIDSQUARE = args.maidenhead
+        if args.notification:
+            self.NOTIFICATION.ENABLED = args.notification == "on"
+        if args.radius:
+            self.SPOTTER_RADIUS = args.radius
+        if args.sked:
+            self.SKED.ENABLED = args.sked == "on"
+        if args.targets:
+            self.TARGETS = self.Parse(args.targets, "C CXN T TXN S SXN", "target")
+
+
+    def _ValidateConfig(self):
+        #
+        # MY_CALLSIGN can be defined in skcc_skimmer.cfg.  It is not required
+        # that it be supplied on the command line.
+        #
+        if not self.MY_CALLSIGN:
+            print("You must specify your callsign, either on the command line or in 'skcc_skimmer.cfg'.")
+            print('')
+            self.Usage()
+
+        if not self.ADI_FILE:
+            print("You must supply an ADI file, either on the command line or in 'skcc_skimmer.cfg'.")
+            print('')
+            self.Usage()
+
+        if not self.GOALS and not self.TARGETS:
+            print('You must specify at least one goal or target.')
+            sys.exit()
+
+        if not self.MY_GRIDSQUARE:
+            print("'MY_GRIDSQUARE' in skcc_skimmer.cfg must be a 4 or 6 character maidenhead grid value.")
+            sys.exit()
+
+        if 'SPOTTER_RADIUS' not in self.configFile:
+            print("'SPOTTER_RADIUS' must be defined in skcc_skimmer.cfg.")
+            sys.exit()
+
+        if 'QUALIFIERS' in self.configFile:
+            print("'QUALIFIERS' is no longer supported and can be removed from 'skcc_skimmer.cfg'.")
+            sys.exit()
+
+        if 'NEARBY' in self.configFile:
+            print("'NEARBY' has been replaced with 'SPOTTERS_NEARBY'.")
+            sys.exit()
+
+        if 'SPOTTER_PREFIXES' in self.configFile:
+            print("'SPOTTER_PREFIXES' has been deprecated.")
+            sys.exit()
+
+        if 'SPOTTERS_NEARBY' in self.configFile:
+            print("'SPOTTERS_NEARBY' has been deprecated.")
+            sys.exit()
+
+        if 'SKCC_FREQUENCIES' in self.configFile:
+            print("'SKCC_FREQUENCIES' is now caluclated internally.  Remove it from 'skcc_skimmer.cfg'.")
+            sys.exit()
+
+        if 'HITS_FILE' in self.configFile:
+            print("'HITS_FILE' is no longer supported.")
+            sys.exit()
+
+        if 'HitCriteria' in self.configFile:
+            print("'HitCriteria' is no longer supported.")
+            sys.exit()
+
+        if 'StatusCriteria' in self.configFile:
+            print("'StatusCriteria' is no longer supported.")
+            sys.exit()
+
+        if 'SkedCriteria' in self.configFile:
+            print("'SkedCriteria' is no longer supported.")
+            sys.exit()
+
+        if 'SkedStatusCriteria' in self.configFile:
+            print("'SkedStatusCriteria' is no longer supported.")
+            sys.exit()
+
+        if 'SERVER' in self.configFile:
+            print('SERVER is no longer supported.')
+            sys.exit()
+
+        if 'SPOT_PERSISTENCE_MINUTES' not in self.configFile:
+            self.SPOT_PERSISTENCE_MINUTES = 15
+
+        if 'GOAL' in self.configFile:
+            print("'GOAL' has been replaced with 'GOALS' and has a different syntax and meaning.")
+            sys.exit()
+
+        if 'GOALS' not in self.configFile:
+            print("GOALS must be defined in 'skcc_skimmer.cfg'.")
+            sys.exit()
+
+        if 'TARGETS' not in self.configFile:
+            print("TARGETS must be defined in 'skcc_skimmer.cfg'.")
+            sys.exit()
+
+        if 'HIGH_WPM' not in self.configFile:
+            print("HIGH_WPM must be defined in 'skcc_skimmer.cfg'.")
+            sys.exit()
+
+        if self.HIGH_WPM.ACTION not in ('suppress', 'warn', 'always-display'):
+            print("HIGH_WPM['ACTION'] must be one of ('suppress', 'warn', 'always-display')")
+            sys.exit()
+
+        if 'OFF_FREQUENCY' not in self.configFile:
+            print("OFF_FREQUENCY must be defined in 'skcc_skimmer.cfg'.")
+            sys.exit()
+
+        if self.OFF_FREQUENCY.ACTION not in ('suppress', 'warn'):
+            print("OFF_FREQUENCY['ACTION'] must be one of ('suppress', 'warn')")
+            sys.exit()
+
+        if 'NOTIFICATION' not in self.configFile:
+            print("'NOTIFICATION' must be defined in skcc_skimmer.cfg.")
+            sys.exit()
+
+    def Usage(self) -> NoReturn:
+        print('Usage:')
+        print('')
+        print('   skcc_skimmer.py')
+        print('                   [--adi <adi-file>]')
+        print('                   [--bands <comma-separated-bands>]')
+        print('                   [--brag-months <number-of-months-back>]')
+        print('                   [--callsign <your-callsign>]')
+        print('                   [--goals <goals>]')
+        print('                   [--help]')
+        print('                   [--interactive]')
+        print('                   [--logfile <logfile-name>]')
+        print('                   [--maidenhead <grid-square>]')
+        print('                   [--notification <on|off>]')
+        print('                   [--radius <distance-in-miles>]')
+        print('                   [--targets <targets>]')
+        print('                   [--verbose]')
+        print(' or...')
+        print('')
+        print('   skcc_skimmer.py')
+        print('                   [-a <adi-file>]')
+        print('                   [-b <comma-separated-bands>]')
+        print('                   [-c <your-callsign>]')
+        print('                   [-g <goals>]')
+        print('                   [-h]')
+        print('                   [-i]')
+        print('                   [-l <logfile-name>]')
+        print('                   [-m <grid-square>]')
+        print('                   [-n <on|off>]')
+        print('                   [-r <distance-in-miles>]')
+        print('                   [-t <targets>]')
+        print('                   [-v]')
+        print('')
+        sys.exit()
+
+    def Parse(self, String: str, ALL_str: str, Type: str) -> list[str]:
+        ALL  = ALL_str.split()
+        List = Split(String.upper())
+
+        for x in List:
+            if x == 'ALL':
+                return ALL
+
+            if x == 'NONE':
+                return []
+
+            if x == 'CXN' and 'C' not in List:
+                List.append('C')
+
+            if x == 'TXN' and 'T' not in List:
+                List.append('T')
+
+            if x == 'SXN' and 'S' not in List:
+                List.append('S')
+
+            if x not in ALL:
+                print(f"Unrecognized {Type} '{x}'.")
+                sys.exit()
+
+        return List
+
 
 class cFastDateTime:
     FastDateTime: str
